490fe65c457490d852fdd8e7dfeb9afc692f8392
trevo <trevo@THATONECOMPUTER>
2026-07-16 11:49:17 +0100

    created the camara class so the player calss does not throw and error but still is just a skeleton and has not implementations

---
+++ My NEA Project/Camara.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_NEA_Project
+{
+    internal class Camara
+    {
+        private int camX;
+        private int camY;
+        private int camWidth;
+        private int camHeight;
+
+        public void ZoomOut()
+        {
+
+        }
+
+        public void MoveCamPos()
+        {
+
+        }
+    }
+}
