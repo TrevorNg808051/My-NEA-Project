873b2cdfcb2a1851acb9315e169b70202c9489d1
trevo <trevo@THATONECOMPUTER>
2026-07-16 11:46:55 +0100

    created the player class have not implemented any of the functions yet so it's stilll a skeleton

---
+++ My NEA Project/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_NEA_Project
+{
+    internal class Player : Creature
+    {
+        private bool gunEquipped;
+
+        public Player(int xCoord, int yCoord, int width, int height, int maximumVerticalVelocity,Camara playerPOV) : base(xCoord, yCoord, width, height, maximumVerticalVelocity)
+        {
+        }
+
+        public override Movement Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
