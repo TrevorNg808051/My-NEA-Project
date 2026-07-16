00de7b029ffd9c7eb8d4f6e78446b1e05763bc00
trevo <trevo@THATONECOMPUTER>
2026-07-16 11:33:50 +0100

    Creature class created with a little change to the UML diagram.
    Ive settled for that creature will not have any horrizontal acceloration as that would look like the creature is on ice
    
    And i also made it so that when a creature jumps they start at their maxVelocity and the velocity will be decreased by gravity which will be implemented in the World class

---
+++ My NEA Project/Creature.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_NEA_Project
+{
+    internal class Creature : Entity
+    {
+        protected int currentVerticalVelocity;
+        protected int maxVerticalSpeed;
+        protected int currentHorrizontalSpeed;
+        protected int horrizontalSpeed;// movement on the horrizontal plain is designed to have no accelortation
+        protected Label equationBar;
+        static protected Random equationGen;
+        protected int answer;
+        protected int jumpingCounter;
+        
+
+        // so a little tweak to the original idea the max vertical speed of the creature will always be positive and independent to the gravity.
+        // also the creature when jumping would start at the max vertical velocity and decelorate to a stop by gravity so acceloration of the crature will be removed and gravity will be handled by the World class
+        public Creature(int xCoord, int yCoord, int width, int height,int maximumVerticalVelocity) : base(xCoord, yCoord, width, height)
+        {
+        }
+        public Movement Move()
+        {
+
+        }
+        public int ReturnCreatureCurrentVerticalVelocity()
+        {
+            return currentVerticalVelocity;
+        }
+        public void Obstruction(bool obstructedVertically)
+        {
+            if (obstructedVertically)
+            {
+                currentVerticalVelocity = 0;
+            }
+            else
+            {
+                currentHorrizontalSpeed = 0;
+            }
+        }
+        public void SetCreatureCoords(int xCoord, int yCoord)
+        {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+        }
+        public string EquationGenerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+ 
+}
