4a2248aebd4897da43a266f8fee32d21d6346392
trevo <trevo@THATONECOMPUTER>
2026-07-16 11:01:56 +0100

    Entity class created according to the UML diagram

---
+++ My NEA Project/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_NEA_Project
+{
+    internal class Entity
+    {
+        protected int xCoord;
+        protected int yCoord;
+        protected int width;
+        protected int height;
+        protected PictureBox entitySprite;
+
+        public Entity(int xCoord,int yCoord, int width, int height)
+        {
+
+        }
+        public int ReturnXCoord()
+        {
+            return xCoord;
+        }
+        public int ReturnYCoord()
+        {
+            return yCoord;
+        }
+        public int ReturnHeight()
+        {
+            return height;
+        }
+        public int returnWidth()
+        {
+            return width;
+        } 
+
+        public void getHit()
+        {
+
+        }
+
+    }
+}
