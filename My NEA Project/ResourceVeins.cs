fbee27839a78a56895f1b139a361b19f2719bf48
trevo <trevo@THATONECOMPUTER>
2026-07-16 12:50:03 +0100

    created the ResourceVein class but not the items class so it's still incoherent

---
+++ My NEA Project/ResourceVeins.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_NEA_Project
+{
+    internal class ResourceVeins: Entity
+    {
+        protected List<Items> listOfDrops;
+        protected int hitPoint;
+        protected int numOfDrops;
+
+        public ResourceVeins(int xCoord, int yCoord, int width, int height) : base(xCoord, yCoord, width, height)
+        {
+        }
+        protected void SetListOfDrops()
+        {
+
+        }
+
+        protected void DropItem()
+        {
+
+        }
+
+        
+    }
+}
