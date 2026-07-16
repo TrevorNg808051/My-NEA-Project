50ca5575091ea3116470e1c9641b5026e4dc63a2
trevo <trevo@THATONECOMPUTER>
2026-07-16 12:45:32 +0100

    created the bullet class also realised that ive inhereted from entity when i should inherit form creature for the other classes

---
+++ My NEA Project/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_NEA_Project
+{
+    internal class Bullet : Entity
+    {
+        private Movement distanceMovedPerFrame;
+        private double answerOfTheBullet;
+        private int timeToLive;
+
+        public Bullet(int xCoord, int yCoord, int width, int height) : base(xCoord, yCoord, width, height)
+        {
+
+        }
+
+        public void FormAnswer(string equation)
+        {
+
+        }
+        public void ReturnAnswerOfBullet()
+        {
+
+        }
+    }
+}
