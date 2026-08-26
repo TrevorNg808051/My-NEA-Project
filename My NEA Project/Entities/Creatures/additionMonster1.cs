4fa510926ef49c8dbeb2d4f414629b179fea3776
trevo <trevo@THATONECOMPUTER>
2026-08-26 09:54:57 +0100

    the monster now can see the player and start running towrads the player but does not currently have any mean of doing damage yet

--- My NEA Project/Entities/Creatures/additionMonster1.cs
+++ My NEA Project/Entities/Creatures/additionMonster1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace My_NEA_Project.Entities.Creatures
 {
@@ -85,7 +87,7 @@ namespace My_NEA_Project.Entities.Creatures
                                 }
                                 else
                                 {
-                                    this.currentVerticalVelocity = 0;
+
                                     this.currentHorrizontalVelocity = horrizontalVelocity;
                                 }
 
@@ -99,7 +101,7 @@ namespace My_NEA_Project.Entities.Creatures
                                 }
                                 else
                                 {
-                                    this.currentVerticalVelocity = 0;
+
                                     this.currentHorrizontalVelocity = horrizontalVelocity * -1;
                                 }
 
@@ -113,14 +115,51 @@ namespace My_NEA_Project.Entities.Creatures
                         Random ran = new Random();
                         goingRight = ran.Next(1, 3) == 2;
                         roamingCounter = 0;
-           
-                        
+
+                       
                     }
+                }
+
+
+                Point playerCoords = worldCreatureIsIn.ReturnPlayerCoords();
+
+                if((Math.Abs(this.xCoord - playerCoords.X) < 12) && (Math.Abs(this.yCoord - playerCoords.Y) < 12))
+                {
+                    roaming = false;
+                    inCombat = true;
                 }
             }
             else if (inCombat)
             {
-                // not today
+                bool playerIsRightOfMonster = this.xCoord - worldCreatureIsIn.ReturnPlayerCoords().X <= 0;
+                if (playerIsRightOfMonster)
+                {
+                    if (this.worldCreatureIsIn.SquareFinder(this.xCoord + this.horrizontalVelocity, this.yCoord).solid)
+                    {
+                        this.currentHorrizontalVelocity = horrizontalVelocity;
+                        this.currentVerticalVelocity = -2;
+                    }
+                    else
+                    {
+
+                        this.currentHorrizontalVelocity = horrizontalVelocity;
+                    }
+                }
+                else if (!playerIsRightOfMonster)
+                {
+                    if (this.worldCreatureIsIn.SquareFinder(this.xCoord - this.horrizontalVelocity, this.yCoord).solid)
+                    {
+                        this.currentHorrizontalVelocity = horrizontalVelocity * -1;
+                        this.currentVerticalVelocity = -2;
+                    }
+                    else
+                    {
+
+                        this.currentHorrizontalVelocity = horrizontalVelocity * -1;
+                    }
+
+                }
+
             }
 
             return new Movement() { horrizontalMovement = this.currentHorrizontalVelocity, verticalMovement = this.currentVerticalVelocity };
--- My NEA Project/Form1.cs
+++ My NEA Project/Form1.cs
@@ -40,7 +40,7 @@ namespace My_NEA_Project
 
             theWorld.AddEntity(thePlayer);
             theWorld.AddEntity(new DebugDummy(0,-20,1,2,10,thePlayerPov,theWorld));
-            theWorld.AddEntity(new AdditionMonster1(10,-10,3,5,10,thePlayerPov,theWorld));
+            theWorld.AddEntity(new AdditionMonster1(100,-10,3,5,10,thePlayerPov,theWorld));
 
             this.Width = Screen.PrimaryScreen.Bounds.Width;
             this.Height = Screen.PrimaryScreen.Bounds.Height;
